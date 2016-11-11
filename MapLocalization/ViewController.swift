b25d1be284cdd64ae8bda88238f4d0e2935609a3
macdt <macdt@macdts-MacBook-Pro.local>
2016-11-11 23:58:25 +0100

    Final commit

--- MapLocalization/ViewController.swift
+++ MapLocalization/ViewController.swift
@@ -7,11 +7,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    
+    
+    var locationManager : CLLocationManager!
+    var myLocations : [CLLocation] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        stopButton.enabled = false
+        startButton.enabled = true
+        clearButton.enabled = false
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        
+        mapView.delegate = self
+        mapView.mapType = MKMapType.Satellite
+        mapView.showsUserLocation = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +47,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startFollow(sender: UIButton) {
+        stopButton.enabled = true;
+        startButton.enabled = false;
+        mapView.showsUserLocation = true
+        locationManager.startUpdatingLocation()
+    }
+    
+    @IBAction func stopFollow(sender: UIButton) {
+        startButton.enabled = true;
+        stopButton.enabled = false;
+        clearButton.enabled = true;
+        locationManager.stopUpdatingLocation();
+        mapView.showsUserLocation = false;
+    }
+
+    @IBAction func clear(sender: UIButton) {
+        clearButton.enabled = false;
+        mapView.removeAnnotations(mapView.annotations)
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        myLocations.append(locations[0] as CLLocation)
+        
+        let speed : CLLocationSpeed = locationManager.location!.speed;
+
+        let newRegion = MKCoordinateRegionMakeWithDistance(locationManager.location!.coordinate, 10 + 40*speed, 10 + 40*speed)
+        mapView.setRegion(newRegion, animated: true)
+        
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = (myLocations.last?.coordinate)!
+        annotation.title = String(format: "%.0f km/h", speed * 3.6)
+        mapView.addAnnotation(annotation)
+    }
 
 }
 
